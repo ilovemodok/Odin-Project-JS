aadc5611103edbfc2776e7ee2adc9fff23fd7b3d
ilovemodok <ilovemodok@hotmail.com>
2023-11-28 21:56:52 +0900

    Sarting new Studies
    This is just the beginning of a refresher to hopfully learn Javascript.

---
+++ javascript.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// let user = "Johnny";
+// let age = 25;
+// let message = "Hey there, suckah!";
+
+// message = "Now this is a party";
+// age = 12;
+
+// console.log(user, age, message);
+
+// let hello = "Hello, Dolly!";
+
+// message = hello;
+
+// console.log(hello);
+// console.log(message);
+
+// const myBirthday = "14.01.1982";
+
+// const COLOR_RED = "#F00";
+
+// let admin, myName;
+
+// myName = "Johnny";
+
+// admin = myName;
+
+// alert(admin);
+
+// let myPlanet = "Earth";
+
+// let currentUserName = "Michiyo";
+
+// let blip = 2;
+
+// console.log(blip);
+
+// let boop = (6 + 5) / blip;
+
+// console.log(boop);
+
+// let x = 100 + 50 * 3;
+
+// console.log(x);
+
+// alert(15 % 9);
+
+// let poop = 10;
+
+// poop += 5;
+
+// console.log(poop);
+
+// let text = "Hello";
+
+// text += " World";
+
+// console.log(text);
+
+// let x = 10;
+
+// x -= 5;
+
+// console.log(x);
+
+// let x = 1;
+// x <<= 5;
+
+// console.log(x);
+
+// let x = "100";
+
+// let y = "5";
+
+// let z = x / y;
+
+// console.log(z);
+
+let x = 100 / "Apple";
+
+console.log(isNaN(x));
+
+let y = new Number(123);
+
+console.log(y);
